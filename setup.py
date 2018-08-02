10ba024c8031d98e56b5e529cccc0b88e2e84d01
Hideo Hattori <hattori@techbureau.jp>
2018-08-02 16:44:14 +0900

    add zaifapi.__version__ and set long_description from README.md

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import io
+import re
 from setuptools import setup, find_packages
+
+
+with io.open('README.md', encoding='utf-8') as fp:
+    readme = fp.read()
+
+with io.open('zaifapi/__init__.py', encoding='utf-8') as fp:
+    version = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
 
 setup(
     name='zaifapi',
-    version='1.6.3',
+    version=version,
     description='Zaif Api Library',
-    long_description='https://pypi.python.org/pypi/zaifapi',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/techbureau/zaifapi',
     author='AkiraTaniguchi, DaikiShiroi',
     author_email='dededededaiou2003@yahoo.co.jp',
